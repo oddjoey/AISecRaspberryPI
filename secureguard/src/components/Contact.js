12fd0a13ba45a6b18f48f6009f09fe7f3a30701f
hamzabdel <hamzaabdel@csu.fullerton.edu>
2025-03-14 15:20:46 -0700

    contact page setup

---
+++ secureguard/src/components/Contact.js
@@ -0,0 +1,12 @@
+// src/components/Live.js
+import React from 'react';
+
+const Contact = () => {
+  return (
+    <div className="contact-page">
+      <h1>Contact</h1>
+    </div>
+  );
+};
+
+export default Contact;
