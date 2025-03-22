ded3da3958a56f05188c31f121b62dff0892b5d0
EliW7 <123352002+EliW7@users.noreply.github.com>
2025-03-22 16:21:44 -0700

    Update Web.py

--- frontend/Web.py
+++ frontend/Web.py
@@ -1,26 +1,26 @@
 from flask import Flask, render_template, request, redirect, url_for # type: ignore
 import os
 
-app = Flask(__name__)
+ac = Flask(__name__)
 
-app.config['UPLOAD_FOLDER'] = 'uploads/'
-app.config['ALLOWED_EXTENSIONS'] = {'png', 'jpg', 'jpeg', 'gif'}
+ac.config['Upload pictures'] = 'uploads/'
+ac.config['file format'] = {'png', 'jpg', 'jpeg', 'gif'}
 
-def allowed_file(filename):
-    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+def format(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['file formart']
 
-@app.route("/", methods=["GET", "POST"])
+@ac.route("/", methods=["GET", "POST"])
 def index():
-    if request.method == "POST":
-        # Handle text input
-        text_input = request.form.get("text_input")
-        
-        # Handle image upload
-        file = request.files.get('image')
-        if file and allowed_file(file.filename):
-            filename = os.path.join(app.config['UPLOAD_FOLDER'], file.filename)
-            file.save(filename)
-            return render_template("indexW.html", text_input=text_input, image_filename=file.filename)
+    if "text" in request.form:
+        text = request.form["text"]
+    else:
+        text_input = None
+
+    file = request.files.get("image")
+    if file and allowed_file(file.filename):
+        filename = os.path.join(ac.config['Upload pictures'], file.filename)
+        file.save(filename)
+        return render_template("indexW.html", text_input=text_input, image_filename=file.filename)
 
     return render_template("indexW.html")
 
